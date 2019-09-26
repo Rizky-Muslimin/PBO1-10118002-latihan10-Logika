8140502d871a621f2560e27b2d6dd8ed81f17981
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:39:36 +0700

    Add files via upload

---
+++ PBO1-10118002-latihan10-Logika/src/pbo1/pkg10118002/latihan10/logika/PBO110118002Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan10.logika;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ */
+public class PBO110118002Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int umur =19;
+    boolean dewasa;
+    dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
